105dbf221c1d5d63d74881a082ac9c546d2ede94
AntonC9018 <anton.curm@gmail.com>
2021-08-07 18:56:27 +0300

    Terminal UI text selection, a rotating hex

--- Game/Assets/Source/Hexagon/Test.cs
+++ Game/Assets/Source/Hexagon/Test.cs
@@ -4,5 +4,42 @@ namespace SomeProject.Hexagon
 {
     public class Test : MonoBehaviour
     {
+        private static readonly float sqrt3_2 = Mathf.Sqrt(3) / 2;
+        private Mesh MakeHex()
+        {
+            var mesh = new Mesh();
+            
+            mesh.vertices = new Vector3[] 
+            { 
+                new Vector3(0, 0, 0),
+                new Vector3(1, 0, 0),
+                new Vector3(0.5f, 0, sqrt3_2),
+                new Vector3(-0.5f, 0, sqrt3_2),
+                new Vector3(-1, 0, 0),
+                new Vector3(-0.5f, 0, -sqrt3_2),
+                new Vector3(0.5f, 0, -sqrt3_2)
+            };
+            mesh.triangles = new int[]
+            {
+                0, 2, 1,
+                0, 3, 2,
+                0, 4, 3,
+                0, 5, 4,
+                0, 6, 5, 
+                0, 1, 6
+            };
+
+            return mesh;
+        }
+
+        private void Start()
+        {
+            GetComponent<MeshFilter>().mesh = MakeHex();
+        }
+
+        private void Update()
+        {
+            transform.Rotate(new Vector3(0, 1, 0), 1.0f);
+        }
     }
 }
