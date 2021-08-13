72e8b484daa6123b033f4a790918d1ebad100305
AntonC9018 <anton.curm@gmail.com>
2021-08-13 16:01:47 +0300

    Plugin template generation from baton

--- python_cli/baton/baton.py
+++ python_cli/baton/baton.py
@@ -1,8 +1,9 @@
 from genericpath import exists
-import click, shutil, os, subprocess, sys
+import click, shutil, os, subprocess, sys, re
 import baton.git_hooks as git_hooks
 from baton.solution import combine_solutions, SolutionFileError
 from baton.logger import log, log_error, log_success, log_warning, log_info, log_reset
+import baton.plugin_template
 
 # Environment variable shenanigans only exist for windows
 import platform
@@ -24,6 +25,8 @@ KARI_GENERATOR_PATH = None       # the built dll path
 PROJECT_DIRECTORY = None         # /
 UNITY_PROJECT_DIRECTORY = None   # /Game
 UNITY_ASSETS_DIRECTORY = None    # /Game/Assets
+KARI_PROJECT_PATH = None         # /Kari
+KARI_PLUGINS_PATH = None         # /Kari/Kari.Plugins
 
 def set_global_and_env(name, value):
     globals()[name] = value
@@ -43,10 +46,9 @@ def cli(build_directory, project_directory):
     set_global("PROJECT_DIRECTORY", project_directory)
     set_global("GIT_SOURCE_HOOKS_PATH", os.path.join(project_directory, "git_hooks"))
     set_global("DOT_GIT_HOOKS_PATH", os.path.join(project_directory, ".git", "hooks"))
-    set_global("KARI_GENERATOR_PATH", 
-        os.path.join(MSBUILD_OUTPUT_PATH, "Kari.Generator", "Release", "netcoreapp3.1", "publish", "kari.exe"))
     set_global("UNITY_PROJECT_DIRECTORY", os.path.join(project_directory, "Game"))
     set_global("UNITY_ASSETS_DIRECTORY", os.path.join(UNITY_PROJECT_DIRECTORY, "Assets"))
+    
 
 @cli.command("setup")
 @click.option("-skip_unity_editor_envvar", is_flag=True, default=False)
@@ -157,29 +159,35 @@ def git_post_checkout():
     copy_github_hooks.callback()
     update_self.callback()
 
+KARI_PLUGIN_NAMES = None
 
 @cli.group("kari")
 def kari():
     """Has to do with code generation"""
-    pass
-
-kari_plugin_names = ["Terminal", "Flags", "UnityHelpers"]
+    set_global("KARI_PROJECT_PATH", os.path.join(PROJECT_DIRECTORY, "Kari"))
+    set_global("KARI_PLUGINS_PATH", os.path.join(KARI_PROJECT_PATH, "Kari.Plugins"))
+    set_global("KARI_GENERATOR_PATH", os.path.join(MSBUILD_OUTPUT_PATH, "Kari.Generator", "Release", "netcoreapp3.1", "publish", "kari.exe"))
+    set_global("KARI_PLUGIN_NAMES", [directory for directory in os.listdir(KARI_PLUGINS_PATH) 
+        if os.path.isdir(os.path.join(KARI_PLUGINS_PATH, directory))])
 
 @kari.command("build")
 @click.option("-clean", is_flag=True, help="Whether to nuke all previous output before recompiling")
 @click.option("-retry", is_flag=True, default=False, help="Whether to retry building a second time if failed")
 @click.option("-debug", is_flag=True, default=False, help="Whether to do a debug build")
-@click.option("-plugin", multiple=True, default=kari_plugin_names, help="Which plugins to also build. May be any of " + str(kari_plugin_names))
-@click.option("-no_plugins", is_flag=True, help="Whether to disregard plugins")
-def build_kari(clean, retry, debug=False, plugin : 'list[str]' = None, no_plugins = False):
+@click.option("-plugin", multiple=True, default=lambda: KARI_PLUGIN_NAMES, help="Which plugins to also build. By default all plugins are built.")
+@click.option("-no_plugins", is_flag=True, help="Whether  to not rebuild any plugins")
+@click.option("-no_generator", is_flag=True, help="Whether to not rebuild the generator")
+def build_kari(clean, retry, debug=False, plugin : 'list[str]' = None, no_plugins = False, no_generator = False):
     """Builds the Kari code generator"""
+
+    log_info(f"Available plugins: {KARI_PLUGIN_NAMES}")
     
     if no_plugins: 
         plugin = []
     elif plugin is None: 
-        plugin = kari_plugin_names
+        plugin = KARI_PLUGIN_NAMES
     else:
-        names_set = set(kari_plugin_names)
+        names_set = set(KARI_PLUGIN_NAMES)
         error = False
         for p in plugin:
             if p not in names_set:
@@ -192,7 +200,7 @@ def build_kari(clean, retry, debug=False, plugin : 'list[str]' = None, no_plugin
     if clean: nuke_kari.callback()
     
     current_directory = os.path.abspath(os.curdir)
-    os.chdir(f"{PROJECT_DIRECTORY}/Kari")
+    os.chdir(KARI_PROJECT_PATH)
 
     try:
         run_sync("dotnet tool restore")
@@ -201,14 +209,17 @@ def build_kari(clean, retry, debug=False, plugin : 'list[str]' = None, no_plugin
         configuration = "Debug" if debug else "Release"
         options = f" --configuration {configuration} --no-self-contained"
 
-        cmds = [f"dotnet publish Kari.Generator/Kari.Generator.csproj{options}"]
+        cmds = []
+
+        if not no_generator:
+            cmds.append(f"dotnet publish Kari.Generator/Kari.Generator.csproj{options}")
 
         for name in plugin:
             cmds.append(f"dotnet publish Kari.Plugins/{name}/{name}.csproj{options}")
 
         # dotnet fails the first time for some strange reason if the repository has just been cloned.
         # Probably has to do with the templates. The sources they generate are not included in the build
-        # the first time 
+        # the first time.
         if retry:
             def run_twice(cmd):
                 try:
@@ -236,6 +247,31 @@ def build_kari(clean, retry, debug=False, plugin : 'list[str]' = None, no_plugin
     
     return True
 
+@kari.command("new_plugin")
+@click.option("-name", required=True, help="The name of the plugin to be added")
+@click.option("-override", is_flag=True, help="Whether to replace the existing plugin folder and all files if the given plugin exists.")
+def new_plugin(name : str, override : bool):
+    """Creates a new plugin, adds it to Kari solution"""
+
+    # Make sure the first letter is capitalized and it is a valid identifier name
+    ident_pattern = re.compile('^[A-Z][A-Za-z0-9_]*')
+
+    if not re.match(ident_pattern, name):
+        log_error(f'{name} is an invalid name. It must start from a capital letter and be a valid identifier')
+        return False
+    
+    if not baton.plugin_template.write(KARI_PLUGINS_PATH, name, override):
+        return False
+
+    try:
+        run_sync(f"dotnet sln add Kari.Plugins/{name}/{name}.csproj")
+    
+    except subprocess.CalledProcessError as err:
+        log_error(f"A subprocess exited with error code {err.returncode}")
+        return False
+
+    return True
+    
 
 @kari.command(name="run", context_settings={"ignore_unknown_options": True})
 @click.option("-rebuild", is_flag=True, help="Whether to recompile Kari before generating code.")
@@ -257,7 +293,7 @@ def generate_with_kari(rebuild, unprocessed_args):
             return False
     
     try:
-        # Pass along all of the unparsed commands
+        # Pass along all of the unparsed arguments
         command = [quote(KARI_GENERATOR_PATH)]
         command.extend(unprocessed_args)
         run_sync(" ".join(command))
@@ -274,7 +310,7 @@ def generate_code_for_unity():
     """Generates code for the unity project """
 
     plugin_path = MSBUILD_OUTPUT_PATH + "/{0}/Release/netcoreapp3.1/Kari.Plugins.{0}.dll"
-    plugins = ",".join([plugin_path.format(p) for p in kari_plugin_names])
+    plugins = ",".join([plugin_path.format(p) for p in KARI_PLUGIN_NAMES])
 
     # TODO: maybe generate in a single file to minimize .meta's, which is possible
     return generate_with_kari.callback(
